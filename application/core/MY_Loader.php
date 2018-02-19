8fbc241192fe09f38dc95d0a189e83d06705ff0c
Alexandre Trindade <alexandredct@gmail.com>
2018-02-19 20:12:03 -0300

    Limpeza

--- application/core/MY_Loader.php
+++
@@ -1,9 +0,0 @@
-<?php if (! defined("BASEPATH")) exit("Não é permitido o acesso direto ao script");
-
-class MY_Loader extends CI_Loader{
-    public function template ($nome, $dados = array()){
-        $this->view("cabecalho.php");
-        $this->view($nome,$dados);
-        $this->view("rodape.php");
-    }
-}
--- application/views/usuarios/novo.php
+++
@@ -1,1 +0,0 @@
-<html>Cadastrado com sucesso.</html>
